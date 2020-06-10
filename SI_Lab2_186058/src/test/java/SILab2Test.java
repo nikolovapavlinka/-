fcefe909ad2aa677bfd66d67522d43d9951ca3d4
nikolovapavlinka <63320787+nikolovapavlinka@users.noreply.github.com>
2020-06-10 23:23:25 +0200

    Update SILab2Test.java

--- SI_Lab2_186058/src/test/java/SILab2Test.java
+++ SI_Lab2_186058/src/test/java/SILab2Test.java
@@ -17,37 +17,34 @@ class SILab2Test {
 
 
         //A,B, D, E.1, E.2, (H, G,F J, K, L, O, E.3, E.2), P, Q
-        //TEST --> 00#
-        assertEquals(createList("0","1","#"), SILab2.function(createList("0","0","#")));
-
+//TEST --> 00##
+        assertEquals(createList("0","1","#","#"), SILab2.function(createList("0","0","#","#")));
 
 
 
         //A,B, D, E.1, E.2, (H, G,F, I, J, K, L, O, E.3, E.2), P, Q
-//TEST --> #0#
-        assertEquals(createList("#","2","#"), SILab2.function(createList("#","0","#")));
+//TEST --> #0##
+        assertEquals(createList("#","2","#","#"), SILab2.function(createList("#","0","#","#")));
 
 
-//A,B, D, E.1, E.2, P, Q
+        //A,B, D, E.1, E.2, P, Q
 //TEST --> NE E MOZHNO
 
 
 
-
         //A,B, D , E.1, E.2, (H, G,F, I, J, L, O, E.3, E.2), P, G
-//TEST --> #00
-        assertEquals(createList("#","1","0"), SILab2.function(createList("#","0","0")));
+//TEST --> #000
+        assertEquals(createList("#","1","0","0"), SILab2.function(createList("#","0","0","0")));
 
 
-//A,B, D, E.1, E.2, (F, M,N, O, E.3, E.2), P, Q
-//TEST --> ##
-        assertEquals(createList("#","#"), SILab2.function(createList("#","#")));
+        //A,B, D, E.1, E.2, (F, M,N, O, E.3, E.2), P, Q
+//TEST --> ####
+        assertEquals(createList("#","#","#","#"), SILab2.function(createList("#","#","#","#")));
 
 
         //A,B, D , E.1, E.2, (H, G,F, J, L, O, E.3, E.2), P, Q
-//TEST --> 000
-        assertEquals(createList("0","0","0"), SILab2.function(createList("0","0","0")));
-
+//TEST --> 0000
+        assertEquals(createList("0","0","0","0"), SILab2.function(createList("0","0","0","0")));
 
 
         //A,B, C, Q
